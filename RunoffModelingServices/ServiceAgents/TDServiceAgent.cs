fafed4e603313ee6ef82ebadd7a7cd33af2f66e3
Tara Gross <taragross@live.com>
2021-08-09 09:31:56 -0600

    test

--- RunoffModelingServices/ServiceAgents/TDServiceAgent.cs
+++ RunoffModelingServices/ServiceAgents/TDServiceAgent.cs
@@ -66,7 +66,7 @@ namespace RunoffModelingServices.ServiceAgents
             }
             catch (Exception ex)
             {
-                return false;
+                throw;
             }
         }//end PostFeatures
 
